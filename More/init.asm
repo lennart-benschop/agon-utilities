b4d54fc7b82c98c5263875ca51f48cdc3df0c63e
Lennart Benschop <lennartb@xs4all.nl>
2022-12-27 13:02:57 +0100

    Added a More utility

---
+++ More/init.asm
@@ -0,0 +1,137 @@
+;
+; Title:	Copy - Initialisation Code
+; Author:	Dean Belfield, Lennart Benschop
+; Created:	06/11/2022
+; Last Updated:	26/12/2022
+;
+; Modinfo:
+; 17/12/2022:	Added parameter processing
+; 26/12/2022:   Adapted to Copy program, use LEA instead of 3x INC IX, Save/restore MB
+
+			SEGMENT CODE
+			
+			XREF	_main
+		
+			.ASSUME	ADL = 1
+				
+			INCLUDE	"equs.inc"
+			
+argv_ptrs_max:		EQU	16			; Maximum number of arguments allowed in argv
+			
+;
+; Start in ADL mode
+;
+			JP	_start			; Jump to start			
+;
+; The header stuff is from byte 64 onwards
+;
+
+_exec_name:		DB	"MORE.BIN", 0		; The executable name, only used in argv
+
+			ALIGN	64
+			
+			DB	"MOS"			; Flag for MOS - to confirm this is a valid MOS command
+			DB	00h			; MOS header version 0
+			DB	01h			; Flag for run mode (0: Z80, 1: ADL)
+;
+; And the code follows on immediately after the header
+;
+_start:			PUSH	AF			; Preserve the registers
+			PUSH	BC
+			PUSH	DE
+			PUSH	IX
+			PUSH	IY
+			LD	A, MB			; Save MB
+			PUSH 	AF
+			XOR 	A
+			LD 	MB, A                   ; Clear to zero so MOS API calls know how to use 24-bit addresses.
+			
+			LD	IX, argv_ptrs		; The argv array pointer address
+			PUSH	IX
+			CALL	_parse_params		; Parse the parameters
+			POP	IX			; IX: argv	
+			LD	B, 0			;  C: argc
+			CALL	_main			; Start user code
+			
+			POP 	AF
+			LD	MB, A
+			POP	IY			; Restore registers
+			POP	IX
+			POP	DE
+			POP	BC
+			POP	AF
+			RET
+	
+; Parse the parameter string into a C array
+; Parameters
+; - HL: Address of parameter string
+; - IX: Address for array pointer storage
+; Returns:
+; -  C: Number of parameters parsed
+;
+_parse_params:		LD	BC, _exec_name
+			LD	(IX+0), BC		; ARGV[0] = the executable name
+			LEA     IX, IX+3
+			CALL	_skip_spaces		; Skip HL past any leading spaces
+;
+			LD	BC, 1			; C: ARGC = 1 - also clears out top 16 bits of BCU
+			LD	B, argv_ptrs_max - 1	; B: Maximum number of argv_ptrs
+;
+_parse_params_1:	
+			PUSH	BC			; Stack ARGC	
+			PUSH	HL			; Stack start address of token
+			CALL	_get_token		; Get the next token
+			LD	A, C			; A: Length of the token in characters
+			POP	DE			; Start address of token (was in HL)
+			POP	BC			; ARGC
+			OR	A			; Check for A=0 (no token found) OR at end of string
+			RET	Z
+;
+			LD	(IX+0), DE		; Store the pointer to the token
+			PUSH	HL			; DE=HL
+			POP	DE
+			CALL	_skip_spaces		; And skip HL past any spaces onto the next character
+			XOR	A
+			LD	(DE), A			; Zero-terminate the token
+			LEA  	IX, IX+3			; Advance to next pointer position
+			INC	C			; Increment ARGC
+			LD	A, C			; Check for C >= A
+			CP	B
+			JR	C, _parse_params_1	; And loop
+			RET
+
+; Get the next token
+; Parameters:
+; - HL: Address of parameter string
+; Returns:
+; - HL: Address of first character after token
+; -  C: Length of token (in characters)
+;
+_get_token:		LD	C, 0			; Initialise length
+$$:			LD	A, (HL)			; Get the character from the parameter string
+			OR	A			; Exit if 0 (end of parameter string in MOS)
+			RET 	Z
+			CP	13			; Exit if CR (end of parameter string in BBC BASIC)
+			RET	Z
+			CP	' '			; Exit if space (end of token)
+			RET	Z
+			INC	HL			; Advance to next character
+			INC 	C			; Increment length
+			JR	$B
+	
+; Skip spaces in the parameter string
+; Parameters:
+; - HL: Address of parameter string
+; Returns:
+; - HL: Address of next none-space character
+;    F: Z if at end of string, otherwise NZ if there are more tokens to be parsed
+;
+_skip_spaces:		LD	A, (HL)			; Get the character from the parameter string	
+			CP	' '			; Exit if not space
+			RET	NZ
+			INC	HL			; Advance to next character
+			JR	_skip_spaces		; Increment length
+
+; Storage for the argv array pointers
+;
+argv_ptrs:		BLKP	argv_ptrs_max, 0			
---
+++ More/main.asm
@@ -0,0 +1,195 @@
+;
+; Title:	More - Main
+; Author:	Lennart Benschop
+; Created:	27/12/2022
+;
+
+			.ASSUME	ADL = 1			
+
+			INCLUDE	"equs.inc"
+			INCLUDE "mos_api.inc"
+
+			SEGMENT CODE
+			
+			XDEF	_main
+
+
+; A simple program to view text files. After each page, show --More--
+;
+; The main routine
+; IXU: argv - pointer to array of parameters
+;   C: argc - number of parameters
+; Returns:
+;  HL: Error code, or 0 if OK
+;
+_main:			LD	A, C
+			CP 	#2
+			JR  	Z, main1
+			LD 	HL, s_USAGE		; Number of args != 3, print usage string and exit
+			CALL	PRSTR
+			LD	HL, 19
+			RET
+main1:			LD 	HL, (IX+3)		; source file name
+			LD	C,  fa_read
+			MOSCALL mos_fopen
+			OR	A
+			JR	NZ, main2	
+			LD	HL, s_ERROR_SRC		; source file not opened, error
+			CALL	PRSTR
+			LD	HL, 4			
+			RET
+main2: 			LD 	C, A    		; Store source file handle to C.
+			MOSCALL mos_sysvars
+			RES 	4, (IX+4)		; Clear mode flag
+			LD 	HL, c_GETMODE
+			CALL	PRCNTSTR  		; Get screen mode parameters.
+main_waitmode:		BIT	4, (IX+4)			
+			JR 	Z, main_waitmode        ; Wait until received.	
+			XOR 	A
+			LD (prev_cr), A
+			LD (current_col), A
+			LD (num_lines), A
+ 			; Load one byte per character.
+main_byte_loop		MOSCALL mos_feof
+			CP 	1
+			JR 	Z, main_load_end
+			MOSCALL mos_fgetc		; Fortunately fgetc transparently passes all bytes.
+			; mos_fgetc does not set carry flag at EOF in MOS 1.02, bummer, therefore separate mos_feof call in the loop.
+			CP	9
+			JR 	NZ, main3
+			; Character is a TAB, expand into spaces, at least one space printed, until column pos is 8-aligned.
+$$:			LD	A, 32
+			RST.LIL 10h
+			CALL	do_advance
+			LD	A,(current_col)
+			AND 	07h
+			JR 	NZ, $B
+			JR	main_not_cr
+main3:			CP      10
+			JR	NZ, main4
+			; Character is LF
+			LD	A, (prev_cr)
+			AND 	A
+			JR	NZ, main_not_cr		; Skip LF when immediately following a CR
+			CALL	do_newline
+			JR	main_not_cr
+main4:			CP	13
+			JR 	NZ, main5
+			; Character is CR
+			CALL	do_newline
+			LD 	A, 1			; Mark last char as CR, so any LF will be skipped.
+			LD	(prev_cr), A
+			JR 	main_byte_loop
+main5			CP	127				
+			JR	Z, main_not_cr		; Ignore DEL char
+			CP 	32
+			JR	C, main_not_cr		; Ignore control chars 0..31, exeept for TAB, CR, LF, tested earlier
+			RST.LIL	10h			; Print the character, all 322--127, 128-255 are printable.
+			CALL	do_advance
+main_not_cr:		XOR 	A
+			LD 	(prev_cr),A
+			JR	main_byte_loop
+main_load_end		MOSCALL mos_fclose 
+			LD	A, (current_col)
+			AND 	A
+			JR 	Z, main6		; Print CRLF if file does not end in newline.
+			LD 	A, 13		
+			RST.LIL 10h
+			LD 	A, 10
+			RST.LIL 10h
+main6:			LD 	HL, 0
+			RET
+
+; Print a counted string (preceded by a count byte)
+; Parameters:
+;  HL: Address of string (24-bit pointer)
+;
+PRCNTSTR             					     
+			LD 	B, (HL)
+			XOR	A
+			CP	B
+			RET 	Z
+			INC 	HL
+$$:			LD 	A, (HL)
+			RST.LIL 10h
+			INC 	HL
+			DJNZ 	$B
+			RET
+			
+; Print a zero-terminated string
+; Parameters:
+;  HL: Address of string (24-bit pointer)
+;
+PRSTR:			LD	A,(HL)
+			OR	A
+			RET	Z
+			RST.LIL	10h
+			INC	HL
+			JR	PRSTR
+
+; Advance the column position. If at end of line, do a newline.
+do_advance:		LD 	A, (current_col)
+			INC	A
+			LD	(current_col), A
+			CP 	(IX+13h) 		; Are we at the end of the screen line?
+			JR	Z, do_newline			
+			RET
+; Do a newline, if rews-1 lines were printe since last pause, do a pause.			
+do_newline:		LD	A, (current_col)
+			CP	(IX+13h)
+			JR	Z, nl_nocrlf		; Skip printing the newline if cursor has advanced to last screen position.
+			LD	A, 13
+			RST.LIL 10h
+			LD	A, 10
+			RST.LIL 10h
+nl_nocrlf:		XOR 	A
+			LD 	(current_col),A		; Clear column position
+			LD	A, (num_lines)
+			INC 	A
+			LD	(num_lines),A
+			INC	A
+			CP	(IX+14h)
+			RET	NZ			; Do we need a pause?
+			XOR 	A
+			LD	(num_lines), A		; Clear line counter
+			LD 	HL, c_MORE
+			CALL 	PRCNTSTR		; Print More message
+$$:			MOSCALL	mos_getkey
+			AND 	A
+			JR	Z, $B
+			CP	'Q'
+			JR	Z, do_quit
+			CP	'q'
+			JR	Z, do_quit
+			CP	27
+			JR	Z, do_quit
+			LD	HL, c_CLEAR
+			CALL	PRCNTSTR
+			RET
+do_quit:		LD	HL, c_CLEAR
+			CALL	PRCNTSTR
+			POP 	HL			; Remove caller address
+			JP	main_load_end
+			
+; Text messages
+;
+s_ERROR_SRC:		DB 	" Cannot open source file\n\r", 0
+s_USAGE:		DB	" Usage: more <txtfile>\n\r", 0                                 
+c_MORE:			DB  	28 ;String length
+			DB	17, 0, 0, 0, 0 ; foregrond black
+			DB 	17,  1, 255,255,255 ; background white
+			DB      "--More--"
+			DB 	17, 0, 255,255,255 ; foreground white
+			DB	17, 1, 0, 0, 0 ; background black
+c_CLEAR			DB      10,13,32,32,32,32,32,32,32,32,13 ; Clear the --More-- output
+c_GETMODE               DB	3, 23,0,6	; Get screen mode parameters.
+
+current_col:		DS	1			; Current column on the screen.
+num_lines:		DS 	1			; Number of lines printed since last pause
+prev_cr:		DS	1			; flag to indicate previous char was CR.
+
+; RAM
+; 
+			DEFINE	LORAM, SPACE = ROM
+			SEGMENT LORAM
+			
